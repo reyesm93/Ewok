5fd84a3f7f37e52b93651d58e54f44f8f8e0bcbb
Arturo Reyes <reyesm93@gmail.com>
2018-01-20 12:34:51 -0600

    Add FUIAuth methods

--- Controllers/LoginVC.swift
+++ Controllers/LoginVC.swift
@@ -9,11 +9,41 @@
 import Foundation
 import UIKit
 import Firebase
+import FirebaseAuthUI
+import FirebaseGoogleAuthUI
+import FirebaseFacebookAuthUI
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController, FUIAuthDelegate {
+    
+    let providers: [FUIAuthProvider] = [
+        FUIGoogleAuth(),
+        FUIFacebookAuth()
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        FirebaseApp.configure()
+        let authUI = FUIAuth.defaultAuthUI()
+        // You need to adopt a FUIAuthDelegate protocol to receive callback
+        authUI?.delegate = self
+        authUI?.providers = providers
+        
+        let authViewController = authUI!.authViewController()
+    }
+    
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        <#code#>
+    }
+    
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?
+        if FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication) ?? false {
+            return true
+        }
+        // other URL handling goes here.
+        return false
     }
 }
