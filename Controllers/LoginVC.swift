69377924c8537f34de39943a5dae9641c0740861
Arturo Reyes <reyesm93@gmail.com>
2018-01-19 20:19:02 -0600

    Add Firebase Auth and Database Pods

---
+++ Controllers/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginVC.swift
+//  Ewok
+//
+//  Created by Arturo Reyes on 1/19/18.
+//  Copyright © 2018 Arturo Reyes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class LoginVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
